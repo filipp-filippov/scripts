7187a9253c32cf4dab08f560f6c884f5a34a7427
Filipp Filippov <8427604+filipp-filippov@users.noreply.github.com>
2021-11-10 18:10:43 +0300

    Update sync_kv.py
    Updated from GUI

--- sync_kv.py
+++ sync_kv.py
@@ -7,61 +7,71 @@ import sys
 import argparse
 
 parser=argparse.ArgumentParser()
-parser.add_argument('--secret', action='store')
-parser.add_argument('--clientid', action='store')
-parser.add_argument('--appid', action='store')
-parser.add_argument('--source', action='store')
-parser.add_argument('--destination', action='store')
-args=parser.parse_args()
+parser.add_argument('--srcTenantId', action='store')
+parser.add_argument('--srcSecret', action='store')
+parser.add_argument('--srcClientId', action='store')
+parser.add_argument('--srcKv', action='store')
+parser.add_argument('--dstTenantId', action='store')
+parser.add_argument('--dstSecret', action='store')
+parser.add_argument('--dstClientId', action='store')
+parser.add_argument('--dstKv', action='store')
+args = parser.parse_args()
 
-#Set vars
+# Set vars
 
 workFolder = os.path.abspath(os.getcwd())
 srcSecretsKeyList = []
 srcSecretsList = []
 srcSecretsDict = {}
 
-#Get bearer
+# Get bearer
 
-payload = {'grant_type': 'client_credentials', 'client_secret': args.secret, 'client_id': args.clientid, 'scope': 'https://vault.azure.net/.default'}
-bToken = requests.post('https://login.microsoftonline.com/9247d46b-02d2-4e53-b9c4-ed4ec9aa859b/oauth2/v2.0/token', payload).json()['access_token']
+
+def get_bearer(clientsecret, clientid, tenantid):
+    payload = {'grant_type': 'client_credentials', 'client_secret': clientsecret, 'client_id': clientid, 'scope': 'https://vault.azure.net/.default'}
+    beartoken = requests.post('https://login.microsoftonline.com/' + tenantid + '/oauth2/v2.0/token', payload).json()['access_token']
+    return beartoken
 
 # List & Dump KV secrets names
 
-kvURL = args.source + '/secrets?api-version=7.2'
-getHeaders = {'Authorization': 'Bearer ' + bToken }
+
+srcBearToken = get_bearer(args.srcSecret, args.srcClientId, args.srcTenantId)
+print(srcBearToken)
+kvURL = args.srcKv + '/secrets?api-version=7.2'
+getHeaders = {'Authorization': 'Bearer ' + srcBearToken}
 srcInitSecretsData = requests.get(kvURL, headers=getHeaders).json()
 
-##Dump
+## Dump
 for secret in srcInitSecretsData['value']:
- try:
-  srcSecretsKeyList.append(secret['id'].split('/')[4])
- except AttributeError:
-  print('!AttrError!')
-  continue
+    try:
+        srcSecretsKeyList.append(secret['id'].split('/')[4])
+    except AttributeError:
+        print('!AttrError!')
+        continue
 
 # Get & Dump KV secrets values
 
 for secret in srcSecretsKeyList:
- secretData = requests.get(args.source + '/secrets/' + secret + '/?api-version=7.2', headers=getHeaders).json()
- secretKey = secretData.get('id').split('/')[4]
- secretValue = secretData.get('value')
- srcSecretsDict[secretKey] = secretValue
+    secretData = requests.get(args.srcKv + '/secrets/' + secret + '/?api-version=7.2', headers=getHeaders).json()
+    secretKey = secretData.get('id').split('/')[4]
+    secretValue = secretData.get('value')
+    srcSecretsDict[secretKey] = secretValue
 
-## Dump
- with open(workFolder + '/srcKvDump', 'a') as srcDumpFile:
-  srcDumpFile.write('%s: %s\n' % (secretKey, secretValue))
- srcDumpFile.close()
+    ## Dump
+    with open(workFolder + '/srcKvDump', 'a') as srcDumpFile:
+        srcDumpFile.write('%s: %s\n' % (secretKey, secretValue))
+    srcDumpFile.close()
 
 print('Secrets successfully exported')
 
 # Create secrets in DST keyvault & Set secrets values
 
-putHeaders = { 'Content-Length': '0', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + bToken }
+dstBearToken = get_bearer(args.dstSecret, args.dstClientId, args.dstTenantId)
+print(dstBearToken)
+putHeaders = {'Content-Length': '0', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + dstBearToken}
 
 for key, value in srcSecretsDict.items():
- secretName = key
- secretPayload = '{\"value\": \"' + value + '\"}'
- response = requests.put(args.destination + 'secrets/' + secretName + '/?api-version=7.2', data=secretPayload, headers=putHeaders)
- print(response.text)
-
+    secretName = key
+    secretPayload = '{\"value\": \"' + value + '\"}'
+    response = requests.put(args.dstKv + 'secrets/' + secretName + '/?api-version=7.2', data=secretPayload, headers=putHeaders)
+    print(response.text)
